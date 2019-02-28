2beeb5630ea5074363316a9c61953ba89ba3afda
Ammanu <wizlove000@gmail.com>
2019-02-28 12:41:10 +0300

    cleaning

--- README.md
+++
@@ -1,3 +0,0 @@
-# Online-Food-Ordering-System-
-Spring Boot project for Web Programming
-Kanban is found on https://trello.com/b/Vb9bE5ao/online-food-ordering-system
--- online_food_ordering/src/main/java/com/seVen/online_food_ordering/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seVen.online_food_ordering.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.seVen.online_food_ordering.security.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-	Role findByname(String name);
-}
